d37f44499be7208312a4111138cb2d2efdb1bc7e
wkingsurin <wkingsurin@gmail.com>
2024-01-11 21:32:08 +0500

    Update

--- src/Gallery/Carousel/Carousel.jsx
+++ src/Gallery/Carousel/Carousel.jsx
@@ -5,26 +5,33 @@ import images from "../../../images.json";
 import { useState } from "react";
 
 export default function Carousel({ body, onMouseEnter, onMouseLeave }) {
-  let imgBlocks = images.map((image, index) => {
-    let name = image.name.split("-").join(" ");
-
-    return (
-      <div key={index} className="image-wrapper">
-        <ImageBlock key={image.src} src={image.src} width={1280} height={720} />
-        <figcaption key={image.name} className="figcaption">
-          {name}
-        </figcaption>
-      </div>
-    );
-  });
-
   return (
     <div
       className="carousel"
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
     >
-      <div className="carousel-items">{imgBlocks}</div>
+      {/* <div className="carousel-items">{imgBlocks}</div> */}
+      <div className="carousel-items">
+        {images.map((image, index) => {
+          let name = image.name.split("-").join(" ");
+
+          return (
+            <div key={index} className="image-wrapper">
+              <ImageBlock
+                key={image.src}
+                src={image.src}
+                width={1280}
+                height={720}
+              />
+              <figcaption key={image.name} className="figcaption">
+                {name}
+              </figcaption>
+            </div>
+          );
+        })}
+      </div>
+
       {/* <ImageBlock src={images[1].src} width={1280} height={720} /> */}
     </div>
   );
